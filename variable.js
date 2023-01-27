bf387ee76d004184720ddf7e6aa5c6d76240550c
msashraf3 <msashrafsingdho@gmail.com>
2023-01-27 11:13:06 +0600

    data types

--- variable.js
+++ variable.js
@@ -1,5 +1,13 @@
+//numeric data types
 var age =69;
 var house =1;
 var weight=69;
 var temperature=69;
 var surprizes=69;
+
+//string data types
+var name='sodor uddin';
+var location="Dhaka";
+
+//boolean data types
+var isOk= true;
