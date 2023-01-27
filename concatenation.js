85136dc87628dfb59b292b8d27a6492130fe2054
msashraf3 <msashrafsingdho@gmail.com>
2023-01-27 14:44:10 +0600

    done

---
+++ concatenation.js
@@ -0,0 +1,22 @@
+var firstName='Kim';
+var lastName='Minji';
+var fullName=firstName+lastName;
+console.log(fullName);
+var fullNameWithSpace=firstName+' '+lastName;
+console.log(fullNameWithSpace);
+
+// string number add
+var number1='10';
+var number2='20';
+var total=number1+number2;
+console.log(total);
+// now the conversion
+var newNumber1=parseInt(number1);
+var newNumber2=parseInt(number2);
+var total2=newNumber1+newNumber2;
+console.log(total2);
+// modulus operator
+var mango=69;
+var person=10;
+var reminder=mango%person;
+console.log(reminder);
